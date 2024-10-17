fe34fa9b069e4996f7678d583910c6c5592ca93f
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-10-17 12:12:47 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,7 +15,7 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo de java manejado desde Github versionado a traves de Git creado por Danitro352");
         
     }
     
