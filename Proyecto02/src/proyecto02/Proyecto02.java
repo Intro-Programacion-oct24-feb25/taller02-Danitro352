fd847124d22ec104186e418a588f2630b1ab9e48
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-10-17 12:20:30 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos basicos de java Versionado a traves de Git Manejado desde GItHub Ademas se usar GitHub Classroom");
+    }
+    
+}
